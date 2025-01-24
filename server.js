26a3ea188db504bb7ecd61c16690fc1429cbe314
ultradaoto <ultradaoto@gmail.com>
2025-01-24 13:11:24 -0800

    port 3100

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const homeRoutes = require('./routes/homeRoutes');
 app.use('/', homeRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
